70b081c2ba11e764d6bf99e6333b1eeb8b4b1297
Arturo <arturohernandezdharak@gmail.com>
2025-02-19 00:48:44 -0600

    Cambio

--- frontend/gestion-tramites-frontend/src/axiosInterceptor.js
+++ frontend/gestion-tramites-frontend/src/axiosInterceptor.js
@@ -1,12 +1,13 @@
-// src/axiosInterceptor.js
 import axios from 'axios';
 import { API_URL } from './config';
 
 axios.interceptors.request.use(
   (config) => {
+    console.log("Antes de modificar la URL:", config.url); // 🔍 Debugging
     if (config.url && config.url.startsWith('/')) {
       config.url = `${API_URL}${config.url}`;
     }
+    console.log("Después de modificar la URL:", config.url); // 🔍 Debugging
     return config;
   },
   (error) => Promise.reject(error)
