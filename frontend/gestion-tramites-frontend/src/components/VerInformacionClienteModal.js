07beaaf1b194a02348fcc62c1b626746f2e4bbff
Arturo <arturohernandezdharak@gmail.com>
2025-03-04 18:38:14 -0600

    Version 1.9.2

--- frontend/gestion-tramites-frontend/src/components/VerInformacionClienteModal.js
+++ frontend/gestion-tramites-frontend/src/components/VerInformacionClienteModal.js
@@ -167,7 +167,7 @@ const VerInformacionClienteModal = ({ open, onClose, cliente, onClienteUpdated }
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {(data.abonos || []).map((abono) => ( 
+                  {abonosData.abonos.map((abono) => (
                     <TableRow key={abono.id}>
                       <TableCell>
                         <strong>{abono.concepto}</strong>
